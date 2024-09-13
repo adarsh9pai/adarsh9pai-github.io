d8c1d9382662e24e76cac5eeeeacebc57a45d280
Adarsh Pai <adarsh9pai@gmail.com>
2024-09-13 07:10:07 +0000

    Updates

---
+++ precache-manifest.d7c858d8ff953652284dbe14d686ffea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a315838aebfbbf2b054",
+    "url": "/static/css/main.18bc8695.chunk.css"
+  },
+  {
+    "revision": "2a315838aebfbbf2b054",
+    "url": "/static/js/main.40a6ad2b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3e7ad2a69ee3f7962e30",
+    "url": "/static/css/2.4e7a24b0.chunk.css"
+  },
+  {
+    "revision": "3e7ad2a69ee3f7962e30",
+    "url": "/static/js/2.3e4582a7.chunk.js"
+  },
+  {
+    "revision": "c8decf4918363e116d6aa0d2dfbff77a",
+    "url": "/index.html"
+  }
+];
